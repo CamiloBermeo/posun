5ae002e1e93a21ddacdf4ef0fb2882c502282fd0
Camilo <bermeocamilo12@gmail.com>
2026-01-22 12:22:45 -0500

    first commit

---
+++ posun-backend/src/main/java/com/posun/PosunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.posun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosunBackendApplication.class, args);
+	}
+
+}
---
+++ posun-backend/src/test/java/com/posun/PosunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
